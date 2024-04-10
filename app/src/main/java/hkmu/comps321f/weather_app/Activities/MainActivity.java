359aa07ab38dbca99c25bca41e0412b772aa9e55
louisakck <151057081+louisakck@users.noreply.github.com>
2024-04-10 13:11:26 +0800

    Dark/Light mode added

--- app/src/main/java/hkmu/comps321f/weather_app/Activities/MainActivity.java
+++ app/src/main/java/hkmu/comps321f/weather_app/Activities/MainActivity.java
@@ -2,11 +2,15 @@ package hkmu.comps321f.weather_app.Activities;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.*;
+
+import android.content.Context;
 import android.content.Intent;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -38,16 +42,48 @@ public class MainActivity extends AppCompatActivity {
     LinearLayout currentLayout;
     //private List<CurrentDetail> list;
 
-
+    //color theme
+    Switch switcher;
+    boolean nightMODE;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    //-------------------------------------------
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //----------------color theme----------------------------
+        switcher = findViewById(R.id.switcher);
+        sharedPreferences = getSharedPreferences("MODE", Context.MODE_PRIVATE);
+        nightMODE = sharedPreferences.getBoolean("night", false);
+
+        if(nightMODE){
+            switcher.setChecked(true);
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+
+        switcher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                if (nightMODE){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    editor = sharedPreferences.edit();
+                    editor.putBoolean("night", false);
+                }else{
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    editor = sharedPreferences.edit();
+                    editor.putBoolean("night", true);
+                }
+                editor.apply();
+            }
+        });
+        //----------------------------------
         currentLayout = findViewById(R.id.cLayout);
 
         date = findViewById(R.id.date);
         todayWeather = findViewById(R.id.todayWeatherText);
         currentTemp = findViewById(R.id.currentTemp);
+
         tempRange = findViewById(R.id.tempRangeText);
         rain = findViewById(R.id.rainText);
         wind = findViewById(R.id.windText);
